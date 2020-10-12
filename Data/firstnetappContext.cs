a023c87b2a7d2b5e01f657819252bad68143bdcf
ifeoluwajoshua <oluwadahunsiifeoluwa02@gmail.com>
2020-10-12 15:55:20 -0600

    first backend code with csharp

---
+++ Data/firstnetappContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using firstnetapp.Models;
+
+namespace firstnetapp.Data
+{
+    public class firstnetappContext : DbContext
+    {
+        public firstnetappContext (DbContextOptions<firstnetappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<firstnetapp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using firstnetapp.Data;
+using firstnetapp.Models;
+
+namespace firstnetapp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly firstnetapp.Data.firstnetappContext _context;
+
+        public IndexModel(firstnetapp.Data.firstnetappContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
